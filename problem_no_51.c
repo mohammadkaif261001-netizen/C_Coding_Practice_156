a3cca07b9e70f18e6754b55d3740b9c945659b1b
Mohammad Kaif <mohammadkaif261001@gmail.com>
2025-11-08 12:23:33 +0600

    Add files via upload

---
+++ problem_no_51.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows, num;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    num = rows * (rows + 1) / 2;
+
+    for (i = rows; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num--;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
