caed40d18b18c9cb4c84503c91134d8a5af5dcbf
Mohammad Kaif <mohammadkaif261001@gmail.com>
2025-11-09 12:05:24 +0600

    Add files via upload

---
+++ problem_no_83.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int mat1[rows][cols], mat2[rows][cols], sum[rows][cols];
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            scanf("%d", &mat1[i][j]);
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            scanf("%d", &mat2[i][j]);
+
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            sum[i][j] = mat1[i][j] + mat2[i][j];
+
+    printf("Sum of matrices:\n");
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+            printf("%d ", sum[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
