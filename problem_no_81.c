92b8629d72e9e660ba854a1fba31e58832bd9acd
Mohammad Kaif <mohammadkaif261001@gmail.com>
2025-11-09 11:58:31 +0600

    Add files via upload

--- problem_no_81.c
+++ problem_no_81.c
@@ -2,29 +2,33 @@
 
 int main()
 {
-    int rows, cols, i, j;
+    int n, i, j;
+    printf("Enter the order of matrix (2 or 3): ");
+    scanf("%d", &n);
 
-    printf("Enter number of rows and columns: ");
-    scanf("%d %d", &rows, &cols);
-
-    int mat[rows][cols], transpose[cols][rows];
-
+    int mat[n][n];
     printf("Enter elements of the matrix:\n");
-    for (i = 0; i < rows; i++)
-        for (j = 0; j < cols; j++)
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
             scanf("%d", &mat[i][j]);
 
-    for (i = 0; i < rows; i++)
-        for (j = 0; j < cols; j++)
-            transpose[j][i] = mat[i][j];
+    int det = 0;
 
-    printf("Transpose of the matrix:\n");
-    for (i = 0; i < cols; i++)
+    if (n == 2)
     {
-        for (j = 0; j < rows; j++)
-            printf("%d ", transpose[i][j]);
-        printf("\n");
+        det = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
     }
+    else if (n == 3)
+    {
+        det = mat[0][0] * (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]) - mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0]) + mat[0][2] * (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
+    }
+    else
+    {
+        printf("Only 2x2 or 3x3 matrices supported.\n");
+        return 0;
+    }
+
+    printf("Determinant = %d", det);
 
     return 0;
 }
