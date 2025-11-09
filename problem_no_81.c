723a6dfd2487563ab0579dc67e251d3c75d4f81d
Mohammad Kaif <mohammadkaif261001@gmail.com>
2025-11-09 11:30:20 +0600

    Add files via upload

---
+++ problem_no_81.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int mat[rows][cols], transpose[cols][rows];
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            scanf("%d", &mat[i][j]);
+
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            transpose[j][i] = mat[i][j];
+
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < cols; i++)
+    {
+        for (j = 0; j < rows; j++)
+            printf("%d ", transpose[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
