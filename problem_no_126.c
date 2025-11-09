c33f1e7221edc2d737bf977174079704ed1a5fc1
Mohammad Kaif <mohammadkaif261001@gmail.com>
2025-11-09 22:32:27 +0600

    Add files via upload

---
+++ problem_no_126.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node *copyList(struct Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    struct Node *newNode = createNode(head->data);
+    newNode->next = copyList(head->next);
+    return newNode;
+}
+
+void printList(struct Node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    struct Node *head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+
+    printf("Original list: ");
+    printList(head);
+
+    struct Node *copied = copyList(head);
+    printf("Copied list: ");
+    printList(copied);
+
+    return 0;
+}
